aadda4acb8c6afc901f195688362af05a03e74c6
DorianA <dorian9armijos@gmail.com>
2023-02-26 23:34:24 -0500

    build: backup database

---
+++ backup.sql
@@ -0,0 +1,234 @@
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: localhost    Database: scap
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `casas`
+--
+
+DROP TABLE IF EXISTS `casas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `casas` (
+  `codigo` varchar(15) NOT NULL,
+  `direccion` varchar(80) NOT NULL,
+  `medidor` varchar(45) DEFAULT NULL,
+  `valor_anterior` varchar(50) DEFAULT NULL,
+  `valor_actual` varchar(50) DEFAULT NULL,
+  `id_cliente` varchar(10) NOT NULL,
+  `fecha` datetime NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `casas`
+--
+
+LOCK TABLES `casas` WRITE;
+/*!40000 ALTER TABLE `casas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `casas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `casas_categorias`
+--
+
+DROP TABLE IF EXISTS `casas_categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `casas_categorias` (
+  `id_casas` varchar(15) NOT NULL,
+  `id_categorias` int NOT NULL,
+  PRIMARY KEY (`id_casas`,`id_categorias`),
+  UNIQUE KEY `id_categrias_UNIQUE` (`id_categorias`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `casas_categorias`
+--
+
+LOCK TABLES `casas_categorias` WRITE;
+/*!40000 ALTER TABLE `casas_categorias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `casas_categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorias` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(80) NOT NULL,
+  `max_agua` varchar(45) DEFAULT NULL,
+  `precio` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (1,'prueba','130m3',12),(2,'Familia','30m3',12),(3,'Previo','',154),(4,'Familiar','125',4),(5,'Otro mas','1588',45),(6,'Ot','458',45),(7,'aa','2131',12),(8,'121as','2321',12),(9,'1231','2131231',12312),(10,'1231','123131',2131231),(11,'1213','12',2);
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `cedula` varchar(10) NOT NULL,
+  `nombres` varchar(80) NOT NULL,
+  `apellidos` varchar(80) NOT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  `codigo_sector` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`cedula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES ('0215186665','Mario Jose','Flores Canto','dsadsa','',NULL),('0215899965','Daniel Josue','Paredes Pacha','','',NULL),('0250186665','asda','sadasda','','',NULL),('0258746697','Pedro Jose','Barragan Mora','mmora@gmail.com','0989960587',NULL);
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cobros`
+--
+
+DROP TABLE IF EXISTS `cobros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cobros` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ingreso` int NOT NULL,
+  `fecha` date NOT NULL,
+  `id_casa` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cobros`
+--
+
+LOCK TABLES `cobros` WRITE;
+/*!40000 ALTER TABLE `cobros` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cobros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `egresos`
+--
+
+DROP TABLE IF EXISTS `egresos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `egresos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `detalle` varchar(500) NOT NULL,
+  `fecha` date NOT NULL,
+  `valor` int NOT NULL,
+  `id_usuarios` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `egresos`
+--
+
+LOCK TABLES `egresos` WRITE;
+/*!40000 ALTER TABLE `egresos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `egresos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sectores`
+--
+
+DROP TABLE IF EXISTS `sectores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sectores` (
+  `codigo` varchar(15) NOT NULL,
+  `detalle` varchar(100) NOT NULL,
+  `id_usuarios` int DEFAULT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sectores`
+--
+
+LOCK TABLES `sectores` WRITE;
+/*!40000 ALTER TABLE `sectores` DISABLE KEYS */;
+INSERT INTO `sectores` VALUES ('ALP','Apachaca',NULL);
+/*!40000 ALTER TABLE `sectores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(80) NOT NULL,
+  `clave` mediumtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'dorian@administrador','$2a$10$uJ6zsY5O5divYBrlieUSSOGLjGcEDhoTYXw8O18hORhkKtg1XA236');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-02-26 23:33:52
