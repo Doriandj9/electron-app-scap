c8166dbab05c7e1df04de16359f061eefce54e91
DorianA <dorian9armijos@gmail.com>
2023-03-16 14:42:11 -0500

    feat: database finihs

--- backup-database.sql
+++ backup-database.sql
@@ -238,7 +238,7 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(80) NOT NULL,
   `clave` mediumtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
